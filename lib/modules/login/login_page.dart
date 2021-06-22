77acbbef60ec03c8440523aab167400f0a8dc449
alexzenon <alex.zenon@work.com.br>
2021-06-22 14:55:24 -0300

    initial commit

---
+++ lib/modules/login/login_page.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_images.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/social_login/social_login_button.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: Container(
+        width: size.width,
+        height: size.height,
+        child: Stack(
+          children: <Widget>[
+            Container(
+              width: size.width,
+              height: size.height * 0.36,
+              decoration: BoxDecoration(
+                gradient: RadialGradient(
+                  center: Alignment.bottomCenter,
+                  radius: 0.75,
+                  colors: [
+                    AppColors.gradient,
+                    AppColors.primary,
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              top: 40,
+              left: 0.0,
+              right: 0.0,
+              child: Image.asset(
+                AppImages.person,
+                width: 208,
+                height: 300,
+              ),
+            ),
+            Positioned(
+              top: 250,
+              left: 0.0,
+              right: 0.0,
+              child: ShaderMask(
+                shaderCallback: (rect) {
+                  return LinearGradient(
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                    colors: [AppColors.background, Colors.transparent],
+                  ).createShader(Rect.fromLTRB(0, 0, 0, 90));
+                },
+                blendMode: BlendMode.dstIn,
+                child: Container(
+                  height: 90,
+                  width: 100,
+                  color: AppColors.background,
+                ),
+              ),
+            ),
+            Positioned(
+              left: 0.0,
+              right: 0.0,
+              bottom: size.height * 0.05,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset(AppImages.logomini),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 30,
+                      left: 70,
+                      right: 70,
+                    ),
+                    child: Text(
+                      "Organize seus boletos em um só lugar",
+                      style: TextStyles.titleHome,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 40,
+                      right: 40,
+                      top: 40,
+                    ),
+                    child: SocialLoginButton(
+                      onTap: () {
+                        print('Deu bom');
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/shared/widgets/social_login/social_login_button.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_images.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
+
+class SocialLoginButton extends StatelessWidget {
+  final VoidCallback onTap;
+  const SocialLoginButton({
+    Key? key,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        height: 56,
+        decoration: BoxDecoration(
+          color: AppColors.shape,
+          borderRadius: BorderRadius.circular(5.0),
+          border: Border.fromBorderSide(BorderSide(color: AppColors.stroke)),
+        ),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset(AppImages.google),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    height: 56,
+                    width: 1,
+                    color: AppColors.stroke,
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 4,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Entrar com Google',
+                    style: TextStyles.buttonGray,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
